14f2f42f232ecb79ecfb1b4a3b82e782c6f81881
Dan Dees <dand@appsmiths.com>
2021-03-06 07:06:25 +0700

    UTIL - add-remotes.sh

---
+++ add-remotes.sh
@@ -0,0 +1,6 @@
+git remote add upstream git@github.com:pyenv/pyenv-virtualenv
+git fetch upstream
+git checkout -B upstream upstream/master
+
+git remote add scop git@github.com:scop/pyenv-virtualenv
+git fetch scop
